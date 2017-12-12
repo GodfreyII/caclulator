edbc82437cae20a0002fb76ab41d5c7782ae1893
Devoloper account <dev@minimal.localdomain>
2017-12-12 12:34:57 +0300

    Implemented several buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,51 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton_0, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_1, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_2, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_3, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_4, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_5, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_6, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_7, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_8, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_9, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_PlusMinus, SIGNAL(released()), this, SLOT(unary_operator_pressed()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::digit_pressed()
+{
+    QPushButton * button = (QPushButton*)sender();
+    double labelnumber;
+    QString newlabel;
+
+    labelnumber = (ui->label->text() + button->text()).toDouble();
+    newlabel = QString::number(labelnumber,'g', 15);
+
+    ui->label->setText(newlabel);
+}
+
+void MainWindow::on_pushButton_Decimal_released()
+{
+    ui->label->setText(ui->label->text() + ".");
+}
+
+void MainWindow::unary_operator_pressed()
+{
+    QPushButton * button = (QPushButton*)sender();
+    double labelnumber;
+    QString newlabel;
+    if(button->text() =="+/-");
+    {
+        labelnumber = ui->label->text().toDouble();
+        labelnumber = labelnumber * -1;
+        newlabel = QString::number(labelnumber, 'g', 15);
+        ui->label->setText(newlabel);
+    }
 }
