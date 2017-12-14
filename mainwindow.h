7af65824b4211549de84695ae52248823f151b5c
Devoloper account <dev@minimal.localdomain>
2017-12-14 14:27:06 +0300

    Implemented several buttons.

--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,9 @@ private slots:
     void digit_pressed();
     void on_pushButton_Decimal_released();
     void unary_operator_pressed();
+    void on_pushButton_Clear_released();
+    void on_pushButton_Equals_released();
+    void binary_operation_pressed();
 };
 
 #endif // MAINWINDOW_H
